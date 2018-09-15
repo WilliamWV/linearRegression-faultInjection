7b4d8f5488f2bfb32fbf638907b05743aa93cea3
wwvargas <william.w.vargas@gmail.com>
2018-09-15 14:22:09 -0300

    more than 4000 faults injected

--- run_fi.sh
+++ run_fi.sh
@@ -1,8 +1,9 @@
 make > dump
 cd ../carol-fi
-./fault_injector.py -c ../linearRegression/lreg.conf -i 10
+./fault_injector.py -c ../linearRegression/lreg.conf -i 5
 cd scripts
 ./faultinj_parser.py
+cp scripts_*.csv ../../linearRegression/faultReport
 cd ../../linearRegression
 rm -rf dump
 
