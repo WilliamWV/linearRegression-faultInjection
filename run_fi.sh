f9d420da0032836925a9bf77dab6c2186875355f
wwvargas <william.w.vargas@gmail.com>
2018-09-15 17:44:10 -0300

    Mais 1500 falhas injetadas

--- run_fi.sh
+++ run_fi.sh
@@ -1,6 +1,6 @@
 make > dump
 cd ../carol-fi
-./fault_injector.py -c ../linearRegression/lreg.conf -i 5
+./fault_injector.py -c ../linearRegression/lreg.conf -i 500
 cd scripts
 ./faultinj_parser.py
 cp scripts_*.csv ../../linearRegression/faultReport
