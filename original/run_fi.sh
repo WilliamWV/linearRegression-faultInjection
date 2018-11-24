be67a5ca30fce7e6575b4615fab20e632805fe44
wwvargas <william.w.vargas@gmail.com>
2018-11-23 22:21:25 -0200

    Injeta mais falhas

--- original/run_fi.sh
+++ original/run_fi.sh
@@ -2,7 +2,7 @@ FOLDER=original
 make > dump
 cd ../../carol-fi
 rm -rf logs
-./fault_injector.py -c ../linearRegression/$FOLDER/lreg.conf -i 300
+./fault_injector.py -c ../linearRegression/$FOLDER/lreg.conf -i 500
 for i in $(ls logs); do
 	rsync -av logs/$i/* ../linearRegression/$FOLDER/logs/$i/ > dump
 done
